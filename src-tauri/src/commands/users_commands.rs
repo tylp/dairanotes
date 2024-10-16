6ebf48c50ca3bdd8289d7bd69c69578fedfa897b
Tylp <antoine.plard.dev@proton.me>
2024-10-16 18:28:21 +0200

    Remove todo comment, separate commands in respective module

---
+++ src-tauri/src/commands/users_commands.rs
@@ -0,0 +1,23 @@
+use tauri::{Manager, State};
+
+use crate::{service::Service, user::User, AppState};
+
+#[tauri::command]
+pub async fn user_index(app_handle: tauri::AppHandle) -> Result<Vec<User>, String> {
+    let state: State<'_, AppState> = app_handle.state();
+
+    match state.user_manager().index().await {
+        Ok(user) => Ok(user),
+        Err(_) => Err("failed to list users".to_string()),
+    }
+}
+
+#[tauri::command]
+pub async fn user_show(app_handle: tauri::AppHandle, id: u32) -> Result<User, String> {
+    let state: State<'_, AppState> = app_handle.state();
+
+    match state.user_manager().show(id).await {
+        Ok(user) => Ok(user),
+        Err(_) => Err("note not found".to_string()),
+    }
+}
